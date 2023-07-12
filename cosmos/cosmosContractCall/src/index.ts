c5122a0d068e646430dbfaf869411671cd1dec78
nnoln <mark3dom@mail.ru>
2023-07-12 18:53:15 -0300

    cosmos examples

---
+++ cosmos/cosmosContractCall/src/index.ts
@@ -0,0 +1,115 @@
+import { Squid } from "@0xsquid/sdk";
+import { SigningStargateClient, DeliverTxResponse } from "@cosmjs/stargate";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { fromBech32, toBech32 } from "@cosmjs/encoding";
+
+// Environment
+// add to a file named ".env" to prevent them being uploaded to github
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const mnemonic = process.env.MNEMONIC;
+const axelarRpc = process.env.AXELAR_RPC_ENDPOINT;
+
+// addresses and IDs
+const axelarChainId = "axelar-dojo-1";
+const osmosisChainId = "osmosis-1";
+const uaxlAddressAxelar = "uaxl";
+const uaxlAddressOsmosis =
+  "ibc/903A61A498756EA560B85A85132D3AEE21B5DEDD41213725D22ABF276EA6945E";
+
+// amount of uaxl to send from Axelar to Osmosis
+const amount = "1000000";
+
+const getSDK = () => {
+  const squid = new Squid({
+    baseUrl: "http://localhost:3000", // "https://api.squidrouter.com", // TODO: set proper URL
+  });
+  return squid;
+};
+
+(async () => {
+  const offlineSigner = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+    prefix: "axelar",
+  });
+
+  const signerAddress = (await offlineSigner.getAccounts())[0].address;
+  console.log(`Sender address: ${signerAddress}`);
+
+  const osmosisAddress = deriveCosmosAddress("osmo", signerAddress);
+  console.log(`Receiver address: ${osmosisAddress}`);
+
+  const signer = await SigningStargateClient.connectWithSigner(
+    axelarRpc,
+    offlineSigner
+  );
+
+  // instantiate the SDK
+  const squid = getSDK();
+  // init the SDK
+  await squid.init();
+  console.log("Squid inited");
+
+  const params = {
+    fromChain: axelarChainId,
+    fromToken: uaxlAddressAxelar,
+    fromAmount: amount,
+    cosmosSignerAddress: signerAddress,
+    toChain: osmosisChainId,
+    toToken: uaxlAddressOsmosis,
+    toAddress: osmosisAddress, // this address will be replaced with contract address
+    slippage: 3.0,
+    customCosmosContractCall: {
+      contract:
+        "osmo15jw7xccxaxk30lf4xgag8f7aeg53pgkh74e39rv00xfnymldjaas2fk627",
+      msg: {
+        wasm: {
+          contract:
+            "osmo15jw7xccxaxk30lf4xgag8f7aeg53pgkh74e39rv00xfnymldjaas2fk627",
+          msg: {
+            swap_with_action: {
+              swap_msg: {
+                token_out_min_amount: "61810",
+                path: [
+                  {
+                    pool_id: "812",
+                    token_out_denom: "uosmo",
+                  },
+                  {
+                    pool_id: "678",
+                    token_out_denom:
+                      "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858",
+                  },
+                ],
+              },
+              after_swap_action: {
+                bank_send: {
+                  receiver: osmosisAddress,
+                },
+              },
+              local_fallback_address: osmosisAddress,
+            },
+          },
+        },
+      },
+    },
+  };
+
+  const { route } = await squid.getRoute(params);
+
+  const txInfo = (await squid.executeRoute({
+    signer,
+    signerAddress,
+    route,
+  })) as DeliverTxResponse;
+
+  const txLink = `https://www.mintscan.io/axelar/txs/${txInfo.transactionHash}`;
+  console.log(`Finished! You can find your transaction here: ${txLink}`);
+})();
+
+export const deriveCosmosAddress = (
+  chainPrefix: string,
+  address: string
+): string => {
+  return toBech32(chainPrefix, fromBech32(address).data);
+};
---
+++ cosmos/evmToCosmosSwap/src/index.ts
@@ -0,0 +1,80 @@
+import { ethers } from "ethers";
+import { Squid } from "@0xsquid/sdk";
+
+// Environment
+// add to a file named ".env" to prevent them being uploaded to github
+import * as dotenv from "dotenv";
+dotenv.config();
+const avaxRpcEndpoint = process.env.AVAX_RPC_ENDPOINT;
+const privateKey = process.env.PRIVATE_KEY;
+
+// addresses and IDs
+const avalancheId = 43114;
+const nativeToken = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+const osmosisChainId = "osmosis-1";
+const uosmoAddress = "uosmo";
+
+// amount of AVAX to send (currently 0.01 AVAX (~$0.10))
+const amount = "10000000000000000";
+
+const getSDK = () => {
+  const squid = new Squid({
+    baseUrl: "https://api.squidrouter.com",
+  });
+  return squid;
+};
+
+(async () => {
+  // set up your RPC provider and signer
+  const provider = new ethers.providers.JsonRpcProvider(avaxRpcEndpoint);
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  // instantiate the SDK
+  const squid = getSDK();
+  // init the SDK
+  await squid.init();
+  console.log("Squid inited");
+
+  const { route } = await squid.getRoute({
+    toAddress: signer.address,
+    fromChain: avalancheId,
+    fromToken: nativeToken,
+    fromAmount: amount,
+    toChain: osmosisChainId,
+    toToken: uosmoAddress,
+    slippage: 1,
+    // enableExpress: false, // default is true on all chains except Ethereum
+    // receiveGasOnDestination: true,
+  });
+
+  console.log(route.estimate.toAmount);
+
+  const tx = (await squid.executeRoute({
+    signer,
+    route,
+  })) as ethers.providers.TransactionResponse;
+  const txReceipt = await tx.wait();
+
+  const axelarScanLink =
+    "https://axelarscan.io/gmp/" + txReceipt.transactionHash;
+  console.log(
+    "Finished! Please check Axelarscan for more details: ",
+    axelarScanLink,
+    "\n"
+  );
+
+  console.log(
+    "Track status via API call to: https://api.squidrouter.com/v1/status?transactionId=" +
+      txReceipt.transactionHash,
+    "\n"
+  );
+
+  // It's best to wait a few seconds before checking the status
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  const status = await squid.getStatus({
+    transactionId: txReceipt.transactionHash,
+  });
+
+  console.log("Status: ", status);
+})();
